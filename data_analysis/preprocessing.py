31c53a0ccdbabc18e7ea8b426bad4d3ff531e1ad
ahxmeds <shadabahamed1996@gmail.com>
2024-05-15 19:51:52 +0000

    Update preprocessing code

--- data_analysis/preprocessing.py
+++ data_analysis/preprocessing.py
@@ -1,13 +1,14 @@
 #%%
+#%%
 import os 
 import SimpleITK as sitk 
-import shutil 
 import pydicom 
-from glob import glob 
-import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
 from joblib import Parallel, delayed
+from skimage.transform import resize
+from skimage.morphology import remove_small_holes
+import time
 # %%
 def read_nifti_image(path):
     return sitk.GetArrayFromImage(sitk.ReadImage(path)).T
@@ -15,134 +16,30 @@ def read_nifti_image(path):
 def read_dicom_image(path):
     dc = pydicom.dcmread(path)
     return dc.pixel_array, dc
+
 #%%
-dicomdir = '/mnt/d/Projects/Thyroid_data/DICOM'
+dicomdir = '/data/blobfuse/default/thyroid-segmentation-results/data/dicom/Data_anonymous'
 patientdirs = os.listdir(dicomdir)
-# patientdirs = [
-#     'C_002932',
-#     'C_002947',
-#     'C_003295',
-#     'C_003320',
-#     'I_410723101',
-#     'I_410730105',
-#     'I_410828106',
-#     'I_410909107',
-#     'I_420225104',
-#     'I_420320111',
-#     'I_420407101',
-#     'I_420421106',
-#     'I_420424107',
-#     'I_420525108',
-#     'I_420901104'
-# ]
 stpaths = [os.path.join(dicomdir, d, f'{d}.dcm') for d in patientdirs]
 gtpaths = [os.path.join(dicomdir, d, f'Untitled.nii.gz') for d in patientdirs]
-# %%
-# plot and watch images and masks
-# %%
-savedir = '/home/shadab/Projects/thyroid-segmentation/data_analysis/dicom_figures'
 
+savedir = '/data/blobfuse/default/thyroid-segmentation-results/data/'
+niftisavedir = os.path.join(savedir, 'nifti')
+vizsavedir = os.path.join(savedir, 'visualization')
+os.makedirs(niftisavedir, exist_ok=True)
+os.makedirs(vizsavedir, exist_ok=True)
 #%%
-patientid_list, sex_list = [], []
-stsizeX_list, stsizeY_list, stsizeZ_list = [],[],[]
-stspacingX_list, stspacingY_list, stspacingZ_list = [],[],[]
-gtsizeX_list, gtsizeY_list, gtsizeZ_list = [], [], []
-gtspacingX_list, gtspacingY_list, gtspacingZ_list = [],[],[]
-
-# def process_paths(stpath, gtpath):
-for stpath, gtpath in zip(stpaths, gtpaths):
-    fname = os.path.basename(os.path.dirname(gtpath))
-    print(fname)
-    patientid_list.append(fname)
-    
+def preprocess_one_image(stpath, gtpath):
+    # read dicom, nifti images into numpy arrays 
     st, dc = read_dicom_image(stpath)
-    sex_list.append(dc.PatientSex)
-    stspacingX_list.append(dc['PixelSpacing'][0])
-    stspacingY_list.append(dc['PixelSpacing'][1])
-    stspacingZ_list.append('null')
-
-    if st.ndim == 2:
-        stsizeX_list.append(st.shape[0])
-        stsizeY_list.append(st.shape[1])
-        stsizeZ_list.append('null')
+    gt = read_nifti_image(gtpath)
+    # read spacing from nifti data
+    spacing = sitk.ReadImage(gtpath).GetSpacing()[:-1]
     
-    elif st.ndim == 3:
-        stsizeX_list.append(st.shape[1])
-        stsizeY_list.append(st.shape[2])
-        stsizeZ_list.append(st.shape[0])
-    else:
-        pass
-    
-
-    gtimg = sitk.ReadImage(gtpath)
-    gtsizeX_list.append(gtimg.GetSize()[0])
-    gtsizeY_list.append(gtimg.GetSize()[1])
-    gtsizeZ_list.append(gtimg.GetSize()[2])
-    gtspacingX_list.append(gtimg.GetSpacing()[0])
-    gtspacingY_list.append(gtimg.GetSpacing()[1])
-    gtspacingZ_list.append(gtimg.GetSpacing()[2])
-
-
-# %%
-colnames = [
-    'PatientID',
-    'Sex',
-    'STSizeX',
-    'STSizeY',
-    'STSizeZ',
-    'GTSizeX',
-    'GTSizeY',
-    'GTSizeZ',
-    'STSpacingX',
-    'STSpacingY',
-    'STSpacingZ',
-    'GTSpacingX',
-    'GTSpacingY',
-    'GTSpacingZ'
-]
-data_info = pd.DataFrame(columns=colnames)
-
-data_info['PatientID'] = patientid_list
-data_info['Sex'] = sex_list
-data_info['STSizeX'] = stsizeX_list
-data_info['STSizeY'] = stsizeY_list
-data_info['STSizeZ'] = stsizeZ_list
-data_info['GTSizeX'] = gtsizeX_list
-data_info['GTSizeY'] = gtsizeY_list
-data_info['GTSizeZ'] = gtsizeZ_list
-data_info['STSpacingX'] = stspacingX_list
-data_info['STSpacingY'] = stspacingY_list
-data_info['STSpacingZ'] = stspacingZ_list
-data_info['GTSpacingX'] = gtspacingX_list
-data_info['GTSpacingY'] = gtspacingY_list
-data_info['GTSpacingZ'] = gtspacingZ_list
-data_info.to_csv('datainfo_version01.csv', index=False)
-
-
-    # gt = read_nifti_image(gtpath)
-
-    # fig, ax = plt.subplots(1, 2)
-    # ax[0].imshow(st)
-    # ax[1].imshow(gt[:,:,0].T)
-    # savepath = os.path.join(savedir, f'{fname}.png')
-    # fig.savefig(savepath, dpi=120, bbox_inches='tight')
-    # plt.close(fig)
-
-    # except: 
-    #     failed_cases.append(fname)
-
-# Parallel(n_jobs=4)(delayed(process_paths)(stpath, gtpath) for stpath, gtpath in zip(stpaths, gtpaths))
-
-# %%
-images_dir = '/mnt/d/Projects/Thyroid_data/NIFTI/images'
-labels_dir = '/mnt/d/Projects/Thyroid_data/NIFTI/labels'
-for index, row in data_info.iterrows():
-    ptid = row['PatientID']
-    stpath = os.path.join(dicomdir, f'{ptid}', f'{ptid}.dcm')
-    gtpath = os.path.join(dicomdir, f'{ptid}', f'Untitled.nii.gz')
-
-    st = pydicom.dcmread(stpath).pixel_array 
-
+    # find if the number of dimensions in the ST 
+    # it can be 2D (128,128) or 3D (2, 128, 128) or (2, 256,256)
+    # goal is to get a final 2D image
+    # select the channel indexed 0 when the image is 3D
     if st.ndim == 2:
         st_final = st 
     elif st.ndim == 3:
@@ -150,29 +47,56 @@ for index, row in data_info.iterrows():
     else:
         pass
     
-    gtimg = sitk.ReadImage(gtpath)
-    gt = read_nifti_image(gtpath)
+    # find the final GT array for the two cases (as above)
+    # properly rotate/transpose to get GT in the correct orientation
     if gt.shape[2] == 1:
         gt_final = gt[:,:,0].T
-    elif gt.shape[2] == 2:
-        gt_final = gt[:,:,1].T
-    else:
-        pass
+    elif gt.shape[2] == 2: 
+        gt_final = np.rot90(gt[:,:,1], 3)
+    
+    # resize the images to 128x128 if they are 256x256
+    # update the spacing variable for such images
+    if gt_final.shape[0] == 256:
+        st_final = resize(st_final, (128,128), anti_aliasing=True)
+        gt_final = resize(gt_final, (128,128), order=0, anti_aliasing=False, preserve_range=True)
+        spacing = 2*spacing
 
-    stimg_final = sitk.GetImageFromArray(st_final.T)
-    stimg_final.SetSpacing(gtimg.GetSpacing()[:-1])
-    stimg_final.SetOrigin(gtimg.GetOrigin()[:-1])
-    stimg_final.SetDirection(gtimg.GetDirection()[:-1])
+    # fill holes, because that's Maziar's favourite activity
+    gt_final = remove_small_holes(gt_final, area_threshold=10, connectivity=2).astype(int)
 
-    gtimg_final = sitk.GetImageFromArray(gt_final.T)
-    gtimg_final.SetSpacing(gtimg.GetSpacing()[:-1])
-    gtimg_final.SetOrigin(gtimg.GetOrigin()[:-1])
-    gtimg_final.SetDirection(gtimg.GetDirection()[:-1])
+    # convert the numpy arrays to sitk nifti image class
+    # note: transposing before converting is important
+    stimg = sitk.GetImageFromArray(st_final.T)
+    gtimg = sitk.GetImageFromArray(gt_final.T)
 
-    stsavepath = os.path.join(images_dir, f'{ptid}.nii.gz')
-    gtsavepath = os.path.join(labels_dir, f'{ptid}.nii.gz')
+    # set the values for spacing
+    stimg.SetSpacing(spacing)
+    gtimg.SetSpacing(spacing)
 
-    sitk.WriteImage(stimg_final, stsavepath)
-    gitk.WriteImage(gtimg_final, gtsavepath)
+    # define filenames for final ST and GT nifti images for saving purpose
+    # save them as .nii.gz 
+    # .nii.gz is the compressed version, has a little overhead when opening them
+    # but saves disk space
+    # since the images are already small, it is okay to save them as compressed files
+    fname = os.path.basename(stpath).split('.')[0]
+    stsavepath = os.path.join(niftisavedir, 'images', f'{fname}.nii.gz')
+    gtsavepath = os.path.join(niftisavedir, 'labels', f'{fname}.nii.gz')
+    sitk.WriteImage(stimg, stsavepath)
+    sitk.WriteImage(gtimg, gtsavepath)
 
-    print(f'Done with {index}: {ptid}')
+    # plot and save ST and GT side-by-side for visualization
+    fig, ax = plt.subplots(1,2)
+    ax[0].imshow(st_final)
+    ax[1].imshow(gt_final)
+    vizsavepath = os.path.join(vizsavedir, f'{fname}.png')
+    fig.savefig(vizsavepath, dpi=200, bbox_inches='tight')
+    plt.close('all')
+    print(f'Done with {fname}')
+    
+    
+# %%
+# parallelize the preprocessing on n_jobs CPU cores using joblib module
+start = time.time()
+Parallel(n_jobs=8)(delayed(preprocess_one_image)(stpath, gtpath) for stpath, gtpath in zip(stpaths, gtpaths))
+print(f'Time taken: {(time.time() - start)/60} mins')
+# %%
