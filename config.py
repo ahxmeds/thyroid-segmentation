4b45edc456ad1341b197d52820fdd6b67ead9af6
ahxmeds <shadabahamed1996@gmail.com>
2024-05-15 23:35:26 +0000

    Restore deleted files

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-#%%
 import os 
 
-# path to the directory containing `data` and `results` (this will be created by the pipeline) folders.
-THYROID_SEGMENTATION_FOLDER = '/data/blobfuse/default/thyroid-segmentation-results' 
+THYROID_SEGMENTATION_FOLDER = '' # path to the directory containing `data` and `results` (this will be created by the pipeline) folders.
 
 DATA_FOLDER = os.path.join(THYROID_SEGMENTATION_FOLDER, 'data', 'nifti')
 RESULTS_FOLDER = os.path.join(THYROID_SEGMENTATION_FOLDER, 'results')
 os.makedirs(RESULTS_FOLDER, exist_ok=True)
 WORKING_FOLDER = os.path.dirname(os.path.abspath(__file__))
-# %%
